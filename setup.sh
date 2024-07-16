fcb938c2091eca0f53705fae803c2a1cce6deab6
Ilya Kochik <ilya.kochik@me.com>
2024-07-16 16:17:33 +0100

    init: added submodules init to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Define the environment prefix
-ENV_PREFIX=".conda"
+# Init submodules
+git submodule init
+git submodule update
 
 # Create the Conda environment from environment.yml with the specified prefix
+ENV_PREFIX=".conda"
 echo "Creating Conda environment from environment.yml..."
 conda env create --file environment.yml --prefix $ENV_PREFIX
 
-# Activate the newly created environment
 echo "Activating the Conda environment..."
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate $ENV_PREFIX
@@ -16,7 +17,6 @@ conda activate $ENV_PREFIX
 echo "Setting up Git hooks..."
 cp .github/hooks/pre-commit .git/hooks/pre-commit
 
-# Make sure the pre-commit hook is executable
 chmod +x .git/hooks/pre-commit
 
 echo "Setup completed successfully."
