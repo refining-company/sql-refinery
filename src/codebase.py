0e67e3c8fc26ed3d96f28e7ce47d7307445c0cee
endrit_beqa <ge63qiz@mytum.de>
2024-08-02 15:56:40 +0200

    refactor: use extend instead of append loop

--- src/codebase.py
+++ src/codebase.py
@@ -123,8 +123,7 @@ def to_queries(file: str, node: sql.Node) -> list[Query]:
             ops.append(Op(file=file, node=op_node, columns=op_cols, alias=sql.find_alias(op_node)))
 
         subqueries = to_queries(file, select_node)
-        for subquery in subqueries:
-            queries.append(subquery)
+        queries.extend(subqueries)
 
         query = Query(file=file, node=select_node, sources=tables + subqueries, ops=ops)
         queries.append(query)
