988e006b1c903ca26253f2f29e9f9e5489518ba5
ilyakochik <45572664+ilyakochik@users.noreply.github.com>
2024-10-06 20:05:13 +0100

    git: restructuring folders and moving to venv

--- .github/hooks/post-checkout-update-env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# .github/hooks/update_conda_env.sh
-
-# Update Conda environment if environment.yml exists
-if [ -f environment.yml ]; then
-    conda env update --file environment.yml --prefix ./.conda --prune
-fi
--- .github/hooks/pre-commit-update-env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# .github/hooks/update_conda_env.sh
-
-# Export the current conda environment, remove 'name:' and 'prefix:' lines, and save to environment.yml
-conda env export --from-history --prefix ./.conda | awk '!/^name: / && !/^prefix: /' > environment.yml
-
-# Add the updated environment.yml to the commit
-git add environment.yml
