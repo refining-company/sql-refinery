4aa41604baa8529746d4192e2363c254b28d101f
ilyakochik <45572664+ilyakochik@users.noreply.github.com>
2024-10-11 22:15:07 +0800

    feature: providing first diagnostic message

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,4 +1,3 @@
-import textwrap
 import sys
 import logging
 from pathlib import Path
@@ -18,53 +17,49 @@ pygls.server.logger.addHandler(handler)
 server = pygls.server.LanguageServer(name="sql-refinery-server", version="0.1-dev")
 
 
-def main(codebase_path: str, document: str):
+def main(codebase_path: str, document: str) -> list[lsp.Diagnostic]:
     print(f"Analysing codebase {codebase_path} and document '{document[:100]}'", file=sys.stderr)
 
     current_session = session.Session(codebase_path=codebase_path)
     suggestions = current_session.analyse_document(contents=document)
 
     # debug output
+    diagnostics = []
     for suggestion in suggestions:
-        print(
-            "{file}:{start_row}:{start_col}-{end_row}:{end_col}\n"
-            "{op}\n\n"
-            "Alternatives freq={freq} sim={score}\n"
-            "{alts}\n\n=======================\n".format(
-                file=suggestion.this.file,
-                start_row=suggestion.this.node.start_point.row + 1,
-                start_col=suggestion.this.node.start_point.column + 1,
-                end_row=suggestion.this.node.end_point.row + 1,
-                end_col=suggestion.this.node.end_point.column + 1,
-                op=suggestion.this.node.text.decode("utf-8"),
-                freq=suggestion.reliability,
-                score=suggestion.similarity,
-                alts=textwrap.indent("\n\n".join(n.node.text.decode("utf-8") for n in suggestion.others), prefix="\t"),
+        diagnostic = lsp.Diagnostic(
+            range=lsp.Range(
+                start=lsp.Position(
+                    line=suggestion.this.node.start_point.row,
+                    character=suggestion.this.node.start_point.column,
+                ),
+                end=lsp.Position(
+                    line=suggestion.this.node.end_point.row,
+                    character=suggestion.this.node.end_point.column,
+                ),
             ),
-            file=sys.stderr,
+            message="Potential logical conflict with codebase",
+            severity=lsp.DiagnosticSeverity.Warning,
+            code="Logic conflict",
+            source="SQL Refinery",
         )
+        diagnostics.append(diagnostic)
+    return diagnostics
 
 
 @server.feature(lsp.TEXT_DOCUMENT_DID_OPEN)
 def did_open(params: lsp.DidOpenTextDocumentParams) -> None:
     document = server.workspace.get_text_document(params.text_document.uri)
     print(f"Opened file: {document.uri}", file=sys.stderr)
-    main(sys.argv[1], document.source)
+    diagnostics = main(sys.argv[1], document.source)
+    server.publish_diagnostics(document.uri, diagnostics)
 
 
 @server.feature(lsp.TEXT_DOCUMENT_DID_CHANGE)
 def did_change(params: lsp.DidChangeTextDocumentParams) -> None:
     document = server.workspace.get_text_document(params.text_document.uri)
     print(f"Changed file", file=sys.stderr)
-    main(sys.argv[1], document.source)
-
-
-# def did_open(ls: pygls.server.LanguageServer, params: DidOpenTextDocumentParams):
-#     text_document: TextDocumentItem = params.text_document
-#     print(f"Opened file: {text_document.uri}", file=sys.stderr)
-#     print(f"Server arguments: {sys.argv}", file=sys.stderr)
-#     if len(sys.argv) >= 2:
-#         main(sys.argv[1], sys.argv[2])
+    diagnostics = main(sys.argv[1], document.source)
+    server.publish_diagnostics(document.uri, diagnostics)
 
 
 if __name__ == "__main__":
